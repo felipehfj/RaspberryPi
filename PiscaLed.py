fc1346a015b4ebeba41b4f4a8cd2c597feb47b1a
Felipe Ferreira <felipehfj@gmail.com>
2013-11-10 00:54:13 -0200

    Update PiscaLed.py
    Adicionado desligamento de avisos, mensagens de inicialização e finalização de programa.

--- PiscaLed.py
+++ PiscaLed.py
@@ -5,19 +5,22 @@
 import RPi.GPIO as GPIO, time
 
 modoEndereco = GPIO.BCM   # Modo de enderecamento dos pinos do RPi
-modoIO = GPIO.OUT         # Modo de configuração da porta de Entrada/Saida
+modoIO = GPIO.OUT         # Modo de configuracao da porta de Entrada/Saida
 pinoLed = 18              # Pino do RPi associado ao led
 tempoEspera = 0.5         # Tempo de espera do pisca
 
 try:
   GPIO.setmode(modoEndereco)
+  GPIO.setwarnings(False)
   GPIO.setup(pinoLed, modoIO)
+  print ("Programa iniciado")
 
   while True:
-	  GPIO.output(pinoLed, True)
-	  time.sleep(tempoEspera)
-	  GPIO.output(pinoLed, False)
-	  time.sleep(tempoEspera)
-	 
+          GPIO.output(pinoLed, True)
+          time.sleep(tempoEspera)
+          GPIO.output(pinoLed, False)
+          time.sleep(tempoEspera)
+         
 except KeyboardInterrupt:
   GPIO.cleanup()
+  print ("Programa encerrado")
